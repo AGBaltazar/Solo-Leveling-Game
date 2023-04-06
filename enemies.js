95147c8dbc03fcd209de13e6acb14d797bde394e
Alejandro Garcia Baltazar <agee.baltazar@gmail.com>
2023-04-06 08:13:41 -0700

    Added constructor function and objects

---
+++ enemies.js
@@ -0,0 +1,12 @@
+function Enemy(name, rank, health, speed, attack, image) {
+    this.name = name;
+    this.rank = rank;
+    this.health = health;
+    this.speed = speed;
+    this.attack = attack;
+    this.image = image;
+  }
+
+let enemy1 = new Enemy("Goblin", "Common", 100, 10, 20, "https://example.com/goblin.jpg");
+let enemy2 = new Enemy("Orc", "Rare", 150, 8, 25, "https://example.com/orc.jpg");
+let enemy3 = new Enemy("Dragon", "Legendary", 500, 5, 50, "https://example.com/dragon.jpg");
