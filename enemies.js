35b9a35c6cab1462326167135fe3f4166f06681f
Alejandro Garcia Baltazar <agee.baltazar@gmail.com>
2023-04-14 08:52:26 -0700

    Added player object

--- enemies.js
+++ enemies.js
@@ -10,3 +10,15 @@ function Enemy(name, rank, health, speed, attack, image) {
 let enemy1 = new Enemy("Goblin", "Common", 100, 10, 20, "https://example.com/goblin.jpg");
 let enemy2 = new Enemy("Orc", "Rare", 150, 8, 25, "https://example.com/orc.jpg");
 let enemy3 = new Enemy("Dragon", "Legendary", 500, 5, 50, "https://example.com/dragon.jpg");
+
+
+function Player(name, level, health, attack, speed, experience) {
+  this.name = name;
+  this.level = level;
+  this.health = health;
+  this.attack = attack;
+  this.speed = speed;
+  this.experience = experience;
+}
+
+let player = new Player("Sung-Jin Woo", 1, 100, 10, 5);
